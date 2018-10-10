5f38582e88b1ed25384f05a6c8ab589d5ce76ac7
Sean Walls <sean.walls@gmail.com>
2018-10-09 21:20:25 -0400

    Add project files.

---
+++ SuperHeroDB/Scripts/service.js
@@ -0,0 +1,75 @@
+﻿function getAllHeroes() {
+    $.ajax({
+        url: "Service/SuperHeroService.svc/GetAllHeroes",
+        type: "GET",
+        dataType: "JSON",
+        success: (result) => {
+            heroes = result;
+            drawHeroTable(heroes);
+        },
+        error: (jqxhr, textstatus, errorthrown) => {
+            alert('error ' + errorthrown);
+        }
+    })
+}
+
+function addHero() {
+    let newHero = {
+        "FirstName": $("#addFirstname").val(),
+        "LastName": $("#addLastname").val(),
+        "HeroName": $("#addHeroname").val(),
+        "PlaceOfBirth": $("#addPlaceOfBirth").val(),
+        "Combat" : $("#addCombatPoints").val()
+    };
+
+    $.ajax({
+        url: "Service/SuperHeroService.svc/AddHero",
+        type: "POST",
+        dataType: "JSON",
+        contentType : "application/json",
+        data: JSON.stringify(newHero),
+        success: (result) => {
+            showOverview();
+        },
+        error: (jqxhr, textstatus, errorthrown) => {
+            alert("There was an error adding your hero.");
+        }
+    })
+}
+
+function putHero() {
+    updateHero.FirstName = $("#updateFirstname").val();
+    updateHero.LastName = $("#updateLastname").val();
+    updateHero.HeroName = $("#updateHeroname").val();
+    updateHero.PlaceOfBirth = $("#updatePlaceOfBirth").val();
+    updateHero.Combat = $("#updateCombatPoints").val();
+
+    $.ajax({
+        url: "Service/SuperHeroService.svc/UpdateHero/"+updateHero.Id,
+        type: "PUT",
+        dataType: "JSON",
+        contentType: "application/json",
+        data: JSON.stringify(updateHero),
+        success: (result) => {
+            showOverview();
+        },
+        error: (jqxhr, textstatus, errorthrown) => {
+            alert("There was an error adding your hero.");
+        }
+    })
+}
+
+function deleteHero(id) {
+    $.ajax({
+        url: "Service/SuperHeroService.svc/DeleteHero/"+id,
+        type: "DELETE",
+        dataType: "JSON",        
+        success: (result) => {
+            heroes = result;
+            drawHeroTable(heroes);
+        },
+        error: (jqxhr, textstatus, errorthrown) => {
+            alert('error ' + errorthrown);
+        }
+    })
+}
---
+++ SuperHeroDB/Service/SuperHeroService.svc.cs
@@ -0,0 +1,59 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Web;
+
+namespace SuperHeroDB.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class SuperHeroService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<SuperHero> GetAllHeroes()
+        {
+            return Data.SuperHeroes;
+        }
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json,UriTemplate = "GetHero/{id}")]
+        public SuperHero GetHero(string id)
+        {
+            return Data.SuperHeroes.Find(sh => sh.Id == int.Parse(id));
+        }
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "AddHero", Method = "POST")]
+        public SuperHero AddHero(SuperHero hero)
+        {
+            hero.Id = Data.SuperHeroes.Max(sh => sh.Id) + 1;
+            Data.SuperHeroes.Add(hero);
+            return hero;
+        }
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "UpdateHero/{id}", Method = "PUT")]
+        public SuperHero UpdateHero(SuperHero updatedHero, string id)
+        {
+            SuperHero hero = Data.SuperHeroes.Where(sh => sh.Id == int.Parse(id)).FirstOrDefault();
+            //update the objects
+            hero.FirstName = updatedHero.FirstName;
+            hero.LastName = updatedHero.LastName;
+            hero.PlaceOfBirth = updatedHero.PlaceOfBirth;
+            hero.HeroName = updatedHero.HeroName;
+            hero.Combat = updatedHero.Combat;
+
+            return hero;
+        }
+
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DeleteHero/{id}", Method = "DELETE")]
+        public List<SuperHero> DeleteHero(string id)
+        {
+            Data.SuperHeroes = Data.SuperHeroes.Where(sh => sh.Id != int.Parse(id)).ToList();
+            return Data.SuperHeroes;
+        }
+    }
+}
